839dacab13d3c16e2a4e5d6e11e9ba34b9ed84ff
Ben Blank <ben.blank@gmail.com>
2024-04-10 00:28:37 +0000

    Remove existing DIP switch behavior; I'm repurposing it.

--- keyboards/keychron/q3_max/q3_max.c
+++ keyboards/keychron/q3_max/q3_max.c
@@ -27,17 +27,6 @@
 #define POWER_ON_LED_DURATION 3000
 static uint32_t power_on_indicator_timer;
 
-#ifdef DIP_SWITCH_ENABLE
-bool dip_switch_update_kb(uint8_t index, bool active) {
-    if (index == 0) {
-        default_layer_set(1UL << (active ? 2 : 0));
-    }
-    dip_switch_update_user(index, active);
-
-    return true;
-}
-#endif
-
 void keyboard_post_init_kb(void) {
 #ifdef LK_WIRELESS_ENABLE
     palSetLineMode(P2P4_MODE_SELECT_PIN, PAL_MODE_INPUT);
