c801d307e717962e853f848a4f708efa90b187a3
Ben Blank <ben.blank@gmail.com>
2024-04-10 21:51:47 +0000

    Reduce firmware size by disabling unused features and enabling LTO.

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/benblank/rules.mk
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/benblank/rules.mk
@@ -1,2 +1,16 @@
-DEBUG_ENABLE = no
+BOOTMAGIC_ENABLE = no
+GRAVE_ESC_ENABLE = no
+LTO_ENABLE = yes
+MAGIC_ENABLE = no
 MOUSEKEY_ENABLE = no
+NKRO_ENABLE = no
+SPACE_CADET_ENABLE = no
+
+# The Q3 Max only has three available endpoints, which are normally used by the
+# keyboard, the shared endpoint (for Extra Keys), and Raw HID. To free one up
+# when the console is enabled, the keyboard is moved to the shared port. This
+# potentially makes the keyboard unavailable while the host is booting (e.g.
+# during BIOS setup).
+ifeq ($(strip $(CONSOLE_ENABLE)), yes)
+	KEYBOARD_SHARED_EP = yes
+endif
