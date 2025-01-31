363105ca4555d89f0e647e7da5f0d0adbc27aa5a
Ben "535" Blank <ben.blank@gmail.com>
2025-01-31 15:32:07 -0800

    Replace search key with Scroll Lock, for controlling VMs.

--- keyboards/keychron/q3_max/ansi_encoder/keymaps/benblank/keymap.c
+++ keyboards/keychron/q3_max/ansi_encoder/keymaps/benblank/keymap.c
@@ -23,8 +23,7 @@ enum layers {
     /** The base layer.
      *
      * Uses the encoder for volume/mute and uses the Fn key to activate the FN
-     * layer while held. Scroll Lock and Pause/Break are changed to Search and
-     * Calculator respectively.
+     * layer while held. The Pause/Break key is replaced with Calculator.
      *
      * Caps Lock is indicated by changing its color to red and Caps Word by
      * changing Left Shift's color to green.
@@ -78,13 +77,12 @@ enum layers {
 
 // These aliases are just so the columns still line up.
 #define MGCSYRQ LALT(KC_SYRQ)
-#define SEARCH SGUI(KC_F)
 #define TG_MCTL TG(MEDIA)
 
 const uint16_t PROGMEM keymaps[NUM_LAYERS][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
     [BASE] = LAYOUT_tkl_ansi(
-        KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_MUTE, KC_PSCR, SEARCH , KC_CALC,
+        KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , KC_MUTE, KC_PSCR, KC_SCRL, KC_CALC,
         KC_GRV , KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , KC_MINS, KC_EQL , KC_BSPC, KC_INS , KC_HOME, KC_PGUP,
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_LBRC, KC_RBRC, KC_BSLS, KC_DEL , KC_END , KC_PGDN,
         KC_CAPS, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT,          KC_ENT ,
